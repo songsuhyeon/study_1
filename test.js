d4e6c67fd8ab2ca958200f92f617133445a02ba3
suheyon <ssh1692@likelion.org>
2021-03-23 22:01:29 +0900

    2021 Likelion 1st Study

---
+++ test.js
@@ -0,0 +1,17 @@
+// let a = 1;
+// let text = "1";
+
+// console.log(a == text);  // 오류 - true
+// console.log(a === text); //데이터 타입도 구분 가능 - false
+
+
+// -----------------------------------------------------------------
+
+let a = ['사과', '배', '바나나', '딸기'];
+
+for(let i = 0; i<a.length;i++)
+{
+    console.log(a[i]);
+}
+let num = 1;
+let text = "abcd";
